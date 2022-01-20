9529f1d9008a99265d1d0d9b09cd5a16235c0ad1
Kevin Barrett <kb@kevinbarrett.org>
2022-01-19 21:56:26 -0500

    feat: add remove strikethrough step

--- src/compile/steps/index.ts
+++ src/compile/steps/index.ts
@@ -1,15 +1,17 @@
 import { ConcatenateTextStep } from "./concatenate-text";
-import { StripFrontmatterStep } from "./strip-frontmatter";
 import { PrependTitleStep } from "./prepend-title";
 import { RemoveCommentsStep } from "./remove-comments";
 import { RemoveLinksStep } from "./remove-links";
+import { RemoveStrikethroughsStep } from "./remove-strikethroughs";
+import { StripFrontmatterStep } from "./strip-frontmatter";
 import { WriteToNoteStep } from "./write-to-note";
 
 export const BUILTIN_STEPS = [
   ConcatenateTextStep,
-  StripFrontmatterStep,
   PrependTitleStep,
   RemoveCommentsStep,
   RemoveLinksStep,
+  RemoveStrikethroughsStep,
+  StripFrontmatterStep,
   WriteToNoteStep,
 ];
---
+++ src/compile/steps/remove-strikethroughs.ts
@@ -0,0 +1,40 @@
+import type {
+  CompileInput,
+  CompileManuscriptInput,
+  CompileSceneInput,
+} from "..";
+import {
+  CompileContext,
+  CompileStepKind,
+  makeBuiltinStep,
+} from "./abstract-compile-step";
+
+const STRIKETHROUGH_REGEX = /~~(.*?)~~/gm;
+
+export const RemoveStrikethroughsStep = makeBuiltinStep({
+  id: "remove-strikethroughs",
+  description: {
+    name: "Remove Strikethroughs",
+    description: "Removes struck-through ~~text~~.",
+    availableKinds: [CompileStepKind.Scene, CompileStepKind.Manuscript],
+    options: [],
+  },
+  compile(input: CompileInput, context: CompileContext): CompileInput {
+    if (context.kind === CompileStepKind.Scene) {
+      return (input as CompileSceneInput[]).map((sceneInput) => {
+        return {
+          ...sceneInput,
+          contents: sceneInput.contents.replace(STRIKETHROUGH_REGEX, () => ""),
+        };
+      });
+    } else {
+      return {
+        ...(input as CompileManuscriptInput),
+        contents: (input as CompileManuscriptInput).contents.replace(
+          STRIKETHROUGH_REGEX,
+          () => ""
+        ),
+      };
+    }
+  },
+});
