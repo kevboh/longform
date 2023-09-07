a8ff79aaa443f6eadcd6edb6e6809d5417b170e5
Brendan Campbell-Hartzell <50385408+b-camphart@users.noreply.github.com>
2023-09-07 16:58:48 -0700

    Use absolute path

--- test/compile/steps/remove-links.test.ts
+++ test/compile/steps/remove-links.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from 'vitest'
-import { replaceWikiLinks, replaceExternalLinks } from '../../../src/compile/steps/remove-links'
+import { replaceWikiLinks, replaceExternalLinks } from 'src/compile/steps/remove-links'
 
 describe("Removing Links", () => {
 
