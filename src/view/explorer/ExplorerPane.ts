8ecd5a03cdab75c3b27ccb6e3e29d28352b1ca6e
Brendan Campbell-Hartzell <50385408+b-camphart@users.noreply.github.com>
2023-10-31 10:35:48 -0700

    Move add/ignore scene logic to external function

--- src/view/explorer/ExplorerPane.ts
+++ src/view/explorer/ExplorerPane.ts
@@ -207,7 +207,7 @@ export class ExplorerPane extends ItemView {
         menu.addItem((item) => {
           item.setTitle("Ignore scene");
           item.setIcon("document");
-          item.onClick(() => { });
+          item.onClick(() => ignoreScene(file.name.split(".")[0]));
         })
         // Triggering this event lets other apps insert menu items
         // including Obsidian, giving us lots of stuff for free.
@@ -290,5 +290,76 @@ export class ExplorerPane extends ItemView {
     if (this.explorerView) {
       this.explorerView.$destroy();
     }
+  }
+}
+
+export const getSelectedDraftWithIndex = () => {
+  const draft = get(selectedDraft) as MultipleSceneDraft;
+  const index = get(drafts).findIndex(
+    (d) => d.vaultPath === draft.vaultPath
+  );
+  return { index, draft }
+}
+
+export const addScene = (fileName: string) => {
+  const { index, draft } = getSelectedDraftWithIndex()
+  if (index >= 0 && draft.format === "scenes") {
+    drafts.update((d) => {
+      const targetDraft = d[index] as MultipleSceneDraft;
+      (d[index] as MultipleSceneDraft).scenes = [
+        ...targetDraft.scenes,
+        { title: fileName, indent: 0 },
+      ];
+      (d[index] as MultipleSceneDraft).unknownFiles =
+        targetDraft.unknownFiles.filter((f) => f !== fileName);
+      return d;
+    });
+  }
+}
+
+export const ignoreScene = (fileName: string) => {
+  const { index, draft } = getSelectedDraftWithIndex()
+  if (index >= 0 && draft.format === "scenes") {
+    drafts.update((d) => {
+      const targetDraft = d[index] as MultipleSceneDraft;
+      (d[index] as MultipleSceneDraft).scenes = targetDraft.scenes.filter(it => it.title != fileName);
+      (d[index] as MultipleSceneDraft).ignoredFiles = [
+        ...targetDraft.ignoredFiles,
+        fileName,
+      ];
+      (d[index] as MultipleSceneDraft).unknownFiles =
+        targetDraft.unknownFiles.filter((f) => f !== fileName);
+      return d;
+    });
+  }
+}
+
+export const addAll = () => {
+  const { index, draft } = getSelectedDraftWithIndex()
+  if (index >= 0 && draft.format === "scenes") {
+    drafts.update((d) => {
+      const targetDraft = d[index] as MultipleSceneDraft;
+      (d[index] as MultipleSceneDraft).scenes = [
+        ...targetDraft.scenes,
+        ...targetDraft.unknownFiles.map((f) => ({ title: f, indent: 0 })),
+      ];
+      (d[index] as MultipleSceneDraft).unknownFiles = [];
+      return d;
+    });
+  }
+}
+
+export const ignoreAll = () => {
+  const { index, draft } = getSelectedDraftWithIndex()
+  if (index >= 0 && draft.format === "scenes") {
+    drafts.update((d) => {
+      const targetDraft = d[index] as MultipleSceneDraft;
+      (d[index] as MultipleSceneDraft).ignoredFiles = [
+        ...targetDraft.ignoredFiles,
+        ...targetDraft.unknownFiles,
+      ];
+      (d[index] as MultipleSceneDraft).unknownFiles = [];
+      return d;
+    });
   }
 }
