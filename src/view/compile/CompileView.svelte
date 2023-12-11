b099c1e508a78837bf7fdad36ff7e02612dc081c
Brendan Campbell-Hartzell <50385408+b-camphart@users.noreply.github.com>
2023-12-11 13:16:27 -0800

    Remove unnecessary binding, which caused error
    For some reason, this binding sometimes would have an `undefined` value, which was then
    set in the steps of the current workflow (possibly due to refresh from an unpopulated
    compile step).

--- src/view/compile/CompileView.svelte
+++ src/view/compile/CompileView.svelte
@@ -327,7 +327,7 @@
       >
         <CompileStepView
           ordinal={item.index + 1}
-          bind:step={$workflows[currentWorkflowName].steps[item.index]}
+          step={$workflows[currentWorkflowName].steps[item.index]}
           on:removeStep={() => {
             const newWorkflow = {
               ...$currentWorkflow,
